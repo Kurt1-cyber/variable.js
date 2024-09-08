de160c01adda673ad3eae906d01742d1febf64ca
Kurt1-cyber <delutakurtlouie@gmail.com>
2024-09-08 22:12:07 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+let name = 'Deluta, Kurt Louie D.';
+
+let WhatDoYouWannaBeComeInYourLife = 'Programmer';
+
+let gender = 'male';
+
+let TwitterHandle = '@Kurt10092005';
+
+console.log('What is your Name? ', name);
+console.log('What do you wanna become in your life? ', WhatDoYouWannaBeComeInYourLife);
+console.log('Are you male or female? ', gender);
+console.log('My twitter: ', TwitterHandle);
